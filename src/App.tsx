abfd3dc93591f04c87b2e2cfc73fcb113577932e
Mrshokhrukh <shokhrukhabduvokhidov@gmail.com>
2024-09-21 22:21:20 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import SearchForm from "./components/features/form/SearchForm";
-import BrandsSection from "./components/ui/BrandsSection";
-import HomePage from "./pages/clientPage/HomePage";
+// import HomePage from "./pages/clientPage/HomePage";
 
 type Props = {};
 
 function App({}: Props) {
   return (
     <div className="">
-      <SearchForm />
-      <div className="mt-[73px]">
-        <BrandsSection />
-        <HomePage />
-      </div>
+      <h1>hello</h1>
     </div>
   );
 }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,27 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import MobileMenu from "./MobileMenu";
-import { useEffect, useState } from "react";
 
 type Props = {};
 
 const Layout = ({}: Props) => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth);
+  // const [isMobile, setIsMobile] = useState(window.innerWidth);
 
-  const handleSize = () => {
-    setIsMobile(window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", handleSize);
-    return () => {
-      window.removeEventListener("resize", handleSize);
-    };
-  }, [window.innerWidth]);
+  // const handleSize = () => {
+  //   setIsMobile(window.innerWidth);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleSize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleSize);
+  //   };
+  // }, [window.innerWidth]);
 
   return (
-    <div className="transition-all ease-linear duration-200 pb-16">
-      {isMobile < 768 ? <MobileMenu /> : <Header />}
+    <div className="transition-all ease-linear duration-200">
+      <Header />
       <Outlet />
       <Footer />
     </div>
